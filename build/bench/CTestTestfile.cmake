af0a5e932e7c5ce598e9875a07a1cf9044db38e5
2017libin <1963482624@qq.com>
2022-10-02 19:29:30 +0800

    remove build

--- build/bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chase/code/relic/bench
-# Build directory: /home/chase/code/relic/build/bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/sample/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chase/code/relic/sample
-# Build directory: /home/chase/code/relic/build/sample
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
