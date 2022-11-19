ee622c74ae58e891c1982d999fa8d98825eed02b
Uboan <80829724+Uboan@users.noreply.github.com>
2022-11-19 18:59:50 +0800

    Update debug.h
    add performance test with new method

--- test/debug.h
+++ test/debug.h
@@ -1,9 +1,13 @@
+#pragma once
 #ifndef DEBUG_H
 #define DEBUG_H
 #include <time.h>
-
+#define SECOND_TEST 3
 time_t begin_t, end_t;
 size_t count_t;
+double time_used;
+size_t second_t;
+
 #if 1
 // 测试性能
 #define PERFORMANCE_TEST(prestr, func, times) begin_t = clock();   \
@@ -21,3 +25,69 @@ size_t count_t;
 #endif
 
 #endif
+
+/***************性能测试代码*******************/
+#include <sys/times.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+static int run_t = 0;
+// static int usertime = 1;
+
+#define TM_START        0
+#define TM_STOP         1
+
+#define START        0
+#define STOP         1
+
+void alarmed_t(int sig)
+{
+    signal(SIGALRM, alarmed_t); 
+    run_t = 0;
+}
+
+double App_tminterval(int stop)
+{
+    double ret = 0;
+    struct tms rus;
+    clock_t now = times(&rus);
+    static clock_t tmstart;
+
+    // if (usertime)
+    //     now = rus.tms_utime;
+
+    if (stop == TM_START) {
+        tmstart = now;
+    } else {
+        long int tck = sysconf(_SC_CLK_TCK);
+        ret = (now - tmstart) / (double)tck;
+    }
+    return ret;
+}
+
+// s为STOP时，返回间隔时间
+double TIME_F(int s)
+{
+    double ret = App_tminterval(s);  // 返回
+    if (s == STOP)
+        alarm(0);  // 停止闹钟
+    return ret;
+}
+
+
+
+// 测试性能
+#define PERFORMANCE_TEST_NEW(prestr, func) begin_t = clock();                 \
+	count_t = 0;                                                              \
+	second_t =  SECOND_TEST;                                                  \
+	time_used = 0.0;                                                          \
+	signal(SIGALRM,alarmed_t);                                                \
+	alarm(second_t);                                                          \
+	run_t=1;                                                                  \
+	TIME_F(START);                                                            \
+    for(count_t=0;run_t&&count_t<0x7fffffff;count_t++){                       \
+	    func;                                                                 \
+    }                                                                         \
+	time_used = TIME_F(STOP);                                                 \
+	printf("%s \n \t\t\t run %d times in %.2fs \n",prestr,count_t/second_t,time_used/second_t);
